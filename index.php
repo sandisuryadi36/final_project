a4abdff13c01be1d5666d45a25a90deb0041a74b
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2021-09-19 13:52:40 +0700

    change using sass

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Home</title>
-        <link rel="stylesheet" type="text/css" href="./assets/main.css">
-    </head>
-    <body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./assets/main.css">
+    <title>Home</title>
+</head>
+<body>
         <header>
-            <?php include 'menu.php' ?>
+            <?php include 'menu.html' ?>
         </header>
         <div id="content">
             <h2>Welcome To My Website</h2>
             <h3>Sandi Suryadi</h3>
         </div>
-        <?php include 'footer.php' ?>
-    </body>
+        <?php include 'footer.html' ?>
+</body>
 </html>
--- menu.php
+++
@@ -1,8 +0,0 @@
-<div id="main_menu">
-    <a href="./">Home</a>
-    <a href="./about.php">About</a>
-    <a href="./portfolio.php">Portfolio</a>
-    <a href="./blog.php">Blog</a>
-    <a href="./team.php">Team</a>
-    <a href="./contact.php">Contact</a>
-</div>
