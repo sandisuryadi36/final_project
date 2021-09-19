eb8a9f2d8c7c34b40d4d694a618c6cfe165ee10d
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2021-09-19 16:10:51 +0700

    simplify flles

--- index.php
+++ index.php
@@ -4,17 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./assets/main.css">
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
     <title>Home</title>
 </head>
 <body>
         <header>
-            <?php include 'menu.html' ?>
+            <nav id="menu">
+                <a href="./">Home</a>
+                <a href="#">About</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Blog</a>
+                <a href="#">Team</a>
+                <a href="#">Contact</a>
+            </nav>
         </header>
         <div id="content">
             <h2>Welcome To My Website</h2>
             <h3>Sandi Suryadi</h3>
         </div>
-        <?php include 'footer.html' ?>
+        <footer>
+            <div id="copyright">
+                <p>Copyright &copy 2021 Sandi Suryadi</p>
+            </div>
+        </footer>
 </body>
 </html>
