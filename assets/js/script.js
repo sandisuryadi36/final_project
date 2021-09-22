e1515a5529e8c7c1495038cb14e97385ef4ffbae
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2021-09-22 23:58:37 +0700

    add full functional toggle menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const fadding = document.querySelector('.overlay');
 const body = document.querySelector('body');
+const toggle = document.querySelector('.toggle-menu');
 
 btnHamburger.addEventListener('click', function(){
     console.log('button hamburger clicked')
@@ -11,11 +12,15 @@ btnHamburger.addEventListener('click', function(){
         fadding.classList.add('fade-out');
         header.classList.remove('open');
         body.classList.remove('noScroll');
+        toggle.classList.remove('fade-in')
+        toggle.classList.add('fade-out')
         
     } else {
         header.classList.add('open');
         fadding.classList.remove('fade-out');
         fadding.classList.add('fade-in');
         body.classList.add('noScroll');
+        toggle.classList.add('fade-in')
+        toggle.classList.remove('fade-out')
     }
 })
